251038957db4768a4d597fbb6d6ed6ad8723fb84
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-08 16:46:21 +0200

    Add chart

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function toggleMenu(visible) {
+  document.querySelector('.section__sidebar').classList.toggle('open', visible);
+  document.querySelector('.main').classList.add('sidebar-close');
+  
+}
+  
+document.querySelector('.close-sidebar').addEventListener('click', function (e) {
+// console.log('click: ', e);
+  e.preventDefault();
+  toggleMenu();
+});
+  
+function toggleMobileMenu(visible) {
+  document.querySelector('.section__sidebar').classList.toggle('open', visible);
+  document.querySelector('.main').classList.add('sidebar-close');
+  
+}
+  
+document.querySelector('.mobile-bars').addEventListener('click', function (e) {
+  console.log('click: ', e);
+  e.preventDefault();
+  toggleMobileMenu();
+});
+  
+// chart.js
+  
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    datasets: [{
+      label: 'Signups',
+      data: [230, 350, 400, 270, 540, 300, 490, 390, 510, 110],
+      backgroundColor:'#80BFCA',
+    // yAxisID: 'y-axis-1',
+    }, {
+      label: 'FTD',
+      data: [130, 250, 520, 170, 440, 330, 410, 110, 410, 210],
+      backgroundColor:'#FF9A38',
+    // yAxisID: 'y-axis-1',
+    },
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
