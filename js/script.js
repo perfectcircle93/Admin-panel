cb3a4b8ae2cc30d3ff6e974ca219d6a6df1a019c
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-10 09:44:09 +0200

    Fix chart

--- js/script.js
+++ js/script.js
@@ -24,31 +24,36 @@ document.querySelector('.mobile-bars').addEventListener('click', function (e) {
   
 // chart.js
   
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+  // 1
   type: 'bar',
   data: {
-    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 2
+    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+    // 3
     datasets: [{
-      label: 'Signups',
-      data: [230, 350, 400, 270, 540, 300, 490, 390, 510, 110],
-      backgroundColor:'#80BFCA',
-    // yAxisID: 'y-axis-1',
-    }, {
-      label: 'FTD',
-      data: [130, 250, 520, 170, 440, 330, 410, 110, 410, 210],
-      backgroundColor:'#FF9A38',
-    // yAxisID: 'y-axis-1',
+      // 4
+      label: "Signups",
+      // 5
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      // 6
+      data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
     },
-    ]
+    {
+      label: "FTD",
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+    },
+    {
+      label: "Earned",
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+      // 7
+      hidden: true,
+    }]
   },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
 });
